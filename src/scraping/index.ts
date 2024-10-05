69b80ee5f0a117c1ccda388f0e00cbfb6a5572bf
Augusto Barreira <barreira266@hotmail.com>
2024-10-05 14:40:27 -0300

    Scrapping de dados navegando para página de detalhes

--- src/scraping/index.ts
+++ src/scraping/index.ts
@@ -1,14 +1,20 @@
-import { GetCeepData } from "./ceep/get_ceep_data"
+import { GetCeepData } from "./ceep/get_ceep_data";
+import { GetJudgedSanctioningProcessesData } from "./cvm/get_judged_sanctioning_processes_data";
 
 async function scrapeData() {
+  // CVM
+  const getJudgedSanctioningProcessesData = new GetJudgedSanctioningProcessesData()
+  
+  // CEEP
   const getCeepData = new GetCeepData()
 
   try {
+    const judgedSanctioningProcessesData = await getJudgedSanctioningProcessesData.execute({});
     const ceepData = await getCeepData.execute()
 
-    console.log('ceepData:', ceepData)
-
     //TODO, salvar dados
+    // console.log('judgedSanctioningProcessesData:', judgedSanctioningProcessesData)
+    // console.log('ceepData:', ceepData)
   } catch (error) {
     throw new Error(error as any)
   }
