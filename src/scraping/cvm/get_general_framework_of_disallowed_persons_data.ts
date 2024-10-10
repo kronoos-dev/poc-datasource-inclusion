4e71aa0c4071711dd45a1f1075c593902b8d0a29
Marina <marinaoliveira.r07@gmail.com>
2024-10-10 06:56:07 -0300

    feat: added scrapper for general framework of disallowed persons

---
+++ src/scraping/cvm/get_general_framework_of_disallowed_persons_data.ts
@@ -0,0 +1,46 @@
+import { puppeteerArgs } from "@utils/puppeter.args";
+import puppeteer from "puppeteer";
+
+class GetGeneralFrameworkOfDisallowedPersonsData {
+  getUrl() {
+    return `https://olinda.bcb.gov.br/olinda/servico/Gepad_QuadrosGeraisInternet/versao/v1/odata/QuadroGeralInabilitados?format=text/html`;
+  }
+
+  async execute() {
+    const url = this.getUrl();
+
+    const browser = await puppeteer.launch(puppeteerArgs);
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const content = await page.$$eval("table.table-bordered", (items) => {
+      return items.map((item) => {
+        const [
+          pas,
+          name,
+          cpf,
+          penalty,
+          termInYears,
+          startOfPenaltyTerm,
+          endOfPenaltyTerm,
+        ] = item.querySelectorAll("td");
+
+        return {
+          pas: `${pas.innerText}`,
+          name: `${name.innerText}`,
+          cpf: `${cpf.innerText}`,
+          penalty: `${penalty.innerText}`,
+          termInYears: `${termInYears.innerText}`,
+          startOfPenaltyTerm: `${startOfPenaltyTerm.innerText}`,
+          endOfPenaltyTerm: `${endOfPenaltyTerm.innerText}`,
+        };
+      });
+    });
+
+    await browser.close();
+
+    return content;
+  }
+}
+
+export { GetGeneralFrameworkOfDisallowedPersonsData };
--- src/scraping/index.ts
+++ src/scraping/index.ts
@@ -1,39 +1,50 @@
-import { GetCelebratedDealsData } from "./cgu/get_celebrated_deals"
+import { GetCelebratedDealsData } from "./cgu/get_celebrated_deals";
+import { GetGeneralFrameworkOfDisallowedPersonsData } from "./cvm/get_general_framework_of_disallowed_persons_data";
+import { GetJudgedSanctioningProcessesData } from "./cvm/get_judged_sanctioning_processes_data";
 
 async function scrapeData() {
   // CVM
-  // const getJudgedSanctioningProcessesData = new GetJudgedSanctioningProcessesData()
-  
+  // const getJudgedSanctioningProcessesData =
+  //   new GetJudgedSanctioningProcessesData();
+
+  const getGeneralFrameworkOfDisallowedPersonsData =
+    new GetGeneralFrameworkOfDisallowedPersonsData();
+
   // PGFN
   // const getGeneralActiveDebtData = new GetGeneralActiveDebtData()
 
   // CEEP
   // const getCeepData = new GetCeepData()
-  
+
   // Portal da transparência
   // -> Beneficios ao cidadão
   // const getAuxilioBrasilData = new GetAuxilioBrasilData()
-  
-  // CGU  
-  const getCelebratedDealsData = new GetCelebratedDealsData()
-  
+
+  // CGU
+  //const getCelebratedDealsData = new GetCelebratedDealsData()
 
   try {
-    // const judgedSanctioningProcessesData = await getJudgedSanctioningProcessesData.execute({});
+    // const judgedSanctioningProcessesData =
+    //   await getJudgedSanctioningProcessesData.execute({});
+    const generalFrameworkOfDisallowedPersonsData =
+      await getGeneralFrameworkOfDisallowedPersonsData.execute();
     // const ceepData = await getCeepData.execute()
     // const generalActiveDebtData = await getGeneralActiveDebtData.execute()
     // const auxilioBrasilData = await getAuxilioBrasilData.execute()
-    const celebratedDealsData = await getCelebratedDealsData.execute()
+    //const celebratedDealsData = await getCelebratedDealsData.execute();
 
     //TODO, salvar dados
-    // console.log('judgedSanctioningProcessesData:', judgedSanctioningProcessesData)
+    console.log(
+      "generalFrameworkOfDisallowedPersonsData:",
+      generalFrameworkOfDisallowedPersonsData
+    );
     // console.log('ceepData:', ceepData)
     // console.log('generalActiveDebtData:', generalActiveDebtData)
     // console.log('auxilioBrasilData:', auxilioBrasilData)
-    console.log('celebratedDealsData:', celebratedDealsData)
+    //console.log("celebratedDealsData:", celebratedDealsData);
   } catch (error) {
-    throw new Error(error as any)
+    throw new Error(error as any);
   }
 }
 
-scrapeData()
+scrapeData();
