13675f0edb642c08658359617ed08b230468d3ad
Augusto Barreira <barreira266@hotmail.com>
2024-10-18 13:21:03 -0300

    Adicionando board de filas

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,32 @@
+import { createBullBoard } from 'bull-board';
+import { BullAdapter } from 'bull-board/bullAdapter'; // Updated import
 import express from 'express';
 
+
+
+import { createCeepQueue } from 'queues/createCeep';
+import { createDeliberationsQueue } from 'queues/createDeliberations';
+import { createGeneralFrameworkOfDisallowedPersonsQueue } from 'queues/createGeneralFrameworkOfDisallowedPersons';
+import { createGeneralFrameworkOfPersonsProhibitedFromActingQueue } from 'queues/createGeneralFrameworkOfPersonsProhibitedFromActing';
+import { createJudgedSanctioningProceedingsQueue } from 'queues/createJudgedSanctioningProceedings';
+import { createLeaveTemporaryPenaltiesQueue } from 'queues/createLeaveTemporaryPenalties';
+import { portalDaTransparenciaQueue } from 'queues/portalDaTransparenciaDataInclusion';
 import { mainRoutes } from './http/routes';
 
 const app = express();
+
+// Add BullBoard
+
+const { router: bullBoardRouter } = createBullBoard([
+  new BullAdapter(portalDaTransparenciaQueue),
+  new BullAdapter(createCeepQueue),
+  new BullAdapter(createDeliberationsQueue),
+  new BullAdapter(createGeneralFrameworkOfDisallowedPersonsQueue),
+  new BullAdapter(createGeneralFrameworkOfPersonsProhibitedFromActingQueue),
+  new BullAdapter(createJudgedSanctioningProceedingsQueue),
+  new BullAdapter(createLeaveTemporaryPenaltiesQueue),
+]);
+app.use('/admin/queues', bullBoardRouter);  // Monitor queues at /admin/queues
 
 app.use(express.json());
 app.use(mainRoutes);
